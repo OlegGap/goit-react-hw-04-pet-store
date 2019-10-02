870493defcbb9b1464accb70027c232c21396ae2
oleg <hnapovskiy20@gmail.com>
2019-10-02 13:58:18 +0300

    Updates

--- precache-manifest.86e3bca27268c1e1e277daf9111c52b9.js
+++ precache-manifest.86e3bca27268c1e1e277daf9111c52b9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b25c787122d1b7b4aa8613120d77ca11",
+    "revision": "8df823ea4a12b6424a2655ee33cdc17c",
     "url": "/goit-react-hw-04-pet-store/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-pet-store/static/css/3.fcec17ff.chunk.css"
   },
   {
-    "revision": "864c6623057145d48590",
+    "revision": "ecac516de8d9df343f02",
     "url": "/goit-react-hw-04-pet-store/static/css/4.a0183747.chunk.css"
   },
   {
@@ -20,20 +20,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-pet-store/static/css/6.a192c09c.chunk.css"
   },
   {
-    "revision": "b41aa93eb610e1a3d5fe",
+    "revision": "ddf70e0de3e2a14918db",
     "url": "/goit-react-hw-04-pet-store/static/css/main.0dbe00f2.chunk.css"
   },
   {
-    "revision": "15ebede6b4334d5628a9",
-    "url": "/goit-react-hw-04-pet-store/static/js/2.ff798954.chunk.js"
+    "revision": "722a030d05c71b73f386",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.d49541cd.chunk.js"
   },
   {
     "revision": "a70864119159f168c483",
     "url": "/goit-react-hw-04-pet-store/static/js/3.604e8c5d.chunk.js"
   },
   {
-    "revision": "864c6623057145d48590",
-    "url": "/goit-react-hw-04-pet-store/static/js/4.79767da2.chunk.js"
+    "revision": "ecac516de8d9df343f02",
+    "url": "/goit-react-hw-04-pet-store/static/js/4.090fed9a.chunk.js"
   },
   {
     "revision": "3f7ef5e645386214be99",
@@ -48,11 +48,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-pet-store/static/js/7.8bf36f05.chunk.js"
   },
   {
-    "revision": "b41aa93eb610e1a3d5fe",
-    "url": "/goit-react-hw-04-pet-store/static/js/main.a43e5f77.chunk.js"
+    "revision": "ddf70e0de3e2a14918db",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.616710fa.chunk.js"
   },
   {
-    "revision": "21b726a8c942e7464194",
-    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.cbafcaf9.js"
+    "revision": "edddf2b0aa10a6df8b88",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.f1d8c35d.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-pet-store/precache-manifest.b12c2952739c6e78624daceb32582e42.js"
+  "/goit-react-hw-04-pet-store/precache-manifest.86e3bca27268c1e1e277daf9111c52b9.js"
 );
 
 self.addEventListener('message', (event) => {
